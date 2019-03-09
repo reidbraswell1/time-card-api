d76b330ad2bbb333d84d8092839d079ebb11edb8
reid <reidbras@gmail.com>
2019-03-09 12:03:40 -0600

    Added url's to listen on

--- TimeCard/Program.cs
+++ TimeCard/Program.cs
@@ -12,14 +12,19 @@ namespace TimeCard
 {
     public class Program
     {
+        // RB Added other url's to listen on
+        //static readonly string[] MyUrls = {"http://localhost:5000","http://192.168.1.72:5000"};
+        static readonly string[] MyUrls = {"http://0.0.0.0:5000"};
         public static void Main(string[] args)
         {
-            BuildWebHost(args).Run();
+            BuildWebHost(args)
+            .Run();
         }
 
         public static IWebHost BuildWebHost(string[] args) =>
             WebHost.CreateDefaultBuilder(args)
                 .UseStartup<Startup>()
+                .UseUrls(MyUrls)
                 .Build();
     }
 }
